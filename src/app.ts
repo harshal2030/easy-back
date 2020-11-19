446fa50cf576bcd98a00d0622ac8e0e8737c0b73
Harshal sanghvi <harshal2030@outlook.com>
2020-11-19 22:40:18 +0530

    started with aggregate queries

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import mediaRouter from './routers/multimedia';
 import quizRouter from './routers/quiz';
 import queRouter from './routers/question';
 import studentRouter from './routers/students';
+import resultRouter from './routers/result';
 
 const app: Application = express();
 const server = http.createServer(app);
@@ -23,6 +24,7 @@ app.use('/media', mediaRouter);
 app.use('/quiz', quizRouter);
 app.use('/que', queRouter);
 app.use('/student', studentRouter);
+app.use('/result', resultRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello');
