57b8eb6ee6ebc1c8486a826aaf571bbc5fe93dee
Harshal sanghvi <harshal2030@outlook.com>
2020-12-03 16:51:47 +0530

    changed paths

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,9 +1,9 @@
 import path from 'path';
 
-const avatarPath = path.join(__dirname, '../../media/users/avatar');
-const classImagePath = path.join(__dirname, '../../media/class/images');
-const classVideoPath = path.join(__dirname, '../../media/class/videos');
-const classDocPath = path.join(__dirname, '../../media/class/docs');
+const avatarPath = path.join(__dirname, '../../../media/users/avatar');
+const classImagePath = path.join(__dirname, '../../../media/class/images');
+const classVideoPath = path.join(__dirname, '../../../media/class/videos');
+const classDocPath = path.join(__dirname, '../../../media/class/docs');
 
 export {
   avatarPath, classImagePath, classVideoPath, classDocPath,
