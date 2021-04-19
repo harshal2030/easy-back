636caa0337a19dfdc5331c60160ff02e0c0f2536
Harshal Sanghvi <harshal2030@outlook.com>
2021-04-19 21:43:48 +0530

    fixed regex

--- src/utils/regexPatterns.ts
+++ src/utils/regexPatterns.ts
@@ -1,6 +1,6 @@
 const usernamePattern = /^[A-Za-z]+[A-Za-z0-9_.]+$/;
 const handlePattern = /\B(@[a-zA-Z0-9_.]+\b)(?!;)/gm;
 
-const staticImageExtPattern = /\.(jpeg|jpeg|png)$/i;
+const staticImageExtPattern = /\.(jpeg|jpg|png)$/i;
 
 export { usernamePattern, handlePattern, staticImageExtPattern };
